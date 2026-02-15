9fd8e60d2c7b41ca45fd606c8f9a278302d49920
Varun Goyal <varungoyalkkp1@gmail.com>
2026-02-15 15:22:21 +0530

    feat(email):ticket model and email setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.text());
+
+app.get('/', (req, res) => {
+    res.send("service is up");
+});
+
 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
+
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
